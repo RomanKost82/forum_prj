e03e850e445e6484e5a52bae3f31b7130781bdf1
RomanKost <r79006482751@gmail.com>
2022-10-30 15:58:25 +0300

    finish

--- service/models.py
+++ service/models.py
@@ -4,6 +4,7 @@ from turtle import title
 from django.db import models
 from django.urls import reverse, reverse_lazy
 
+
 class Post(models.Model):
 
     title = models.CharField(verbose_name='название', max_length=250)
@@ -12,18 +13,19 @@ class Post(models.Model):
     created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='дата изменения', auto_now=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f'{self.title}'
 
     def get_absolute_url(self):
         return reverse('index')
+
 
 class Comment(models.Model):
     desc = models.TextField(verbose_name='описание', null=True, blank=True)
     created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f'{self.desc}'
 
     def get_absolute_url(self):
--- service/views.py
+++ service/views.py
@@ -3,55 +3,62 @@ from service.models import Comment, Post
 from django.views.generic import ListView, DeleteView, CreateView, UpdateView, DetailView
 from .forms import PostForm, CommentForm, UserRegisterForm
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 def index(req):
     return render(req, 'index.html')
 
-@login_required
+
 def about(req):
     return render(req, 'about.html')
 
-class RegisterForm(CreateView):
+
+class RegisterForm(SuccessMessageMixin, CreateView):
     form_class = UserRegisterForm
+    success_mesage = "%(username)s was created"
     template_name = 'register.html'
     success_url = reverse_lazy('login')
 
+
 class PostView(ListView):
-    
+
     model = Post
     template_name = "index.html"
     ordering = ['-created_at']
+
 
 class DetailPostView(DetailView):
     model = Post
     template_name = "detail_post.html"
 
-class CreatePostView(LoginRequiredMixin, CreateView):
-    model = Post
-    template_name = "create_post.html"
-    form_class = PostForm
-   
 
-class UpdatePostView(LoginRequiredMixin, UpdateView):
+class CreatePostView(CreateView):
     model = Post
     template_name = "create_post.html"
     form_class = PostForm
+
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = "create_post.html"
+    form_class = PostForm
+
 
 class DeletePostView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = "delete_post.html"
     success_url = reverse_lazy('index')
 
+
 class AddCommentView(LoginRequiredMixin, CreateView):
     model = Comment
     template_name = "add_comment.html"
     form_class = CommentForm
-   
+
     def form_valid(self, form):
         form.instance.post_id = self.kwargs['pk']
         return super().form_valid(form)
-
-
-    
